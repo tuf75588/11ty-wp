be87119a763e978dd30e508695532d25cb68186d
Andrew Davis <atd285@gmail.com>
2023-05-16 16:48:42 -0400

    pro tip: don't use node-fetch with 11ty

---
+++ _data/github.js
@@ -0,0 +1,12 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+module.exports = async function() {
+  // https://developer.github.com/v3/repos/#get
+  let json = await EleventyFetch("https://api.github.com/repos/11ty/eleventy", {
+    duration: "1d", // 1 day
+    type: "json" // also supports "text" or "buffer"
+  });
+  console.log(json)
+  return {
+    stargazers: json.stargazers_count
+  };
+};
